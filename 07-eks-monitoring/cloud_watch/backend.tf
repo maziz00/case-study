69c77e8edf349d733ca66e0c064b8445fb074485
Mohamed Abdel Aziz <maziz@accerlated.net>
2023-08-04 08:30:55 +0400

    modify files

--- 07-eks-monitoring/cloud_watch/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "emirates-eks-terraform-backend-store"
-    key            = "tfstate/eks_cloudwatch"
-    region         = "me-south-1"
-    dynamodb_table = "emirates_infra_builder_state_lock"
-  }
-}
